4d705a8a3e39b28c810b159ca0bb802143f56ef0
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-11 17:25:32 +0100

    form

--- js/login.js
+++ js/login.js
@@ -6,18 +6,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const formData = new FormData(loginForm);
 
-        // Make an AJAX POST request to the custom login endpoint
-        fetch('/wp-json/myapp/v1/login/', {
+        // Define the CORS proxy URL
+        const corsAnywhereUrl = "https://noroffcors.onrender.com/";
+
+        // Specify your WordPress login endpoint URL
+        const loginEndpoint = "https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/login/";
+
+        // Send a POST request using the CORS proxy
+        fetch(corsAnywhereUrl + loginEndpoint, {
             method: 'POST',
             body: formData,
         })
         .then(response => response.json())
         .then(data => {
             if (data.message === 'Authentication successful') {
-                // Redirect to a success page or perform other actions
-                window.location.href = '/success-page/';
+                // Redirect or show a success message
+                window.location.href = '/dashboard'; // Redirect to the dashboard
             } else {
-                // Display an error message to the user
+                // Display an error message
                 alert('Login failed. Please check your credentials.');
             }
         })
