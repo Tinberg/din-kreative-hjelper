39a83e910d971c5a1dfb4f55bacb2a42ddf7e25c
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-11 22:22:56 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,43 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector(".login-form");
+  const form = document.querySelector(".login-form");
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const formData = new FormData(form);
-        const loginData = {
-            username_or_email: formData.get("username_or_email"),
-            password: formData.get("password"),
-        };
+    const formData = new FormData(form);
+    const loginData = {
+      username_or_email: formData.get("username_or_email").toLowerCase(),
+      password: formData.get("password"),
+    };
 
-        // CORS Proxy URL
-        const corsAnywhereUrl = "https://noroffcors.onrender.com/";
-        
-        // Target URL for the login endpoint
-        const targetUrl = "https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/login/";
+    // CORS Proxy URL
+    const corsAnywhereUrl = "https://noroffcors.onrender.com/";
 
-        // Using CORS Proxy for the fetch request
-        fetch(corsAnywhereUrl + targetUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "X-Requested-With": "XMLHttpRequest"
-            },
-            body: JSON.stringify(loginData),
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message === 'Authentication successful') {
-                console.log("User logged in successfully", data);
-                // Handle login success (e.g., redirect to another page)
-            } else {
-                console.error("Login error", data);
-                // Display login errors to the user
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
-    });
+    // Target URL for the login endpoint
+    const targetUrl =
+      "https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/login/";
+
+    // Using CORS Proxy for the fetch request
+    fetch(corsAnywhereUrl + targetUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+      body: JSON.stringify(loginData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "Authentication successful") {
+          console.log("User logged in successfully", data);
+          // Handle login success (e.g., redirect to another page)
+        } else {
+          console.error("Login error", data);
+          // Display login errors to the user
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 });
