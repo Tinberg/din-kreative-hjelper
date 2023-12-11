1cfe73a13ed973a28e86f84f4624b24d5258fba0
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-11 12:40:09 +0100

    form

---
+++ js/new_user.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".create-user-form form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const userData = {
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    fetch(
+      "https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/register/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(userData),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.id) {
+          console.log("User created successfully", data);
+          // Handle success (e.g., redirect to login page or show success message)
+        } else {
+          console.error("Error creating user", data);
+          // Handle errors (e.g., show error message to the user)
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+});
