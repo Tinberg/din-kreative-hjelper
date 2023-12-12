a92ef43d65288bf52328862735b006be5bf75c20
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-12 10:43:52 +0100

    Update user_page.js

--- js/user_page.js
+++ js/user_page.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const corsAnywhereUrl = "https://noroffcors.onrender.com/";
+    const userProfileUrl = 'https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile/';
+
     // Fetch user profile data
-    fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile/')
+    fetch(corsAnywhereUrl + userProfileUrl)
       .then(response => response.json())
       .then(data => {
         if (data.username) {
           // Display username, email, and products
           document.getElementById('username').textContent = data.username;
           document.getElementById('email').textContent = data.email;
-          // Assuming you have a div for products
           const productsDiv = document.getElementById('products');
           data.products_for_sale.forEach(product => {
-            // Create and append elements for each product
-            // Modify this according to how you want to display products
             const productElement = document.createElement('div');
             productElement.textContent = product.name; // Adjust based on your product structure
             productsDiv.appendChild(productElement);
@@ -21,11 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
       .catch(error => {
         console.error('Error:', error);
       });
-  });
-  
+});
 
-  document.addEventListener("DOMContentLoaded", function() {
-    fetch('/wp-json/myapp/v1/check-session')
+document.addEventListener("DOMContentLoaded", function() {
+    const corsAnywhereUrl = "https://noroffcors.onrender.com/";
+    const checkSessionUrl = 'https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/check-session';
+
+    // Check user session
+    fetch(corsAnywhereUrl + checkSessionUrl)
       .then(response => response.json())
       .then(data => {
         if (data.status !== 'authenticated') {
@@ -36,5 +39,4 @@ document.addEventListener("DOMContentLoaded", function() {
       .catch(error => {
         console.error('Error:', error);
       });
-  });
-  
+});
