e28c169f051d53e9b7a647fc4d887e6f02369ae1
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-12 11:17:19 +0100

    form

--- js/login.js
+++ js/login.js
@@ -45,40 +45,40 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".login-form");
-  
+
     form.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      const formData = new FormData(form);
-      const loginData = {
-        username_or_email: formData.get("username_or_email").toLowerCase(),
-        password: formData.get("password"),
-      };
-  
-      const corsAnywhereUrl = "https://noroffcors.onrender.com/";
-      const targetUrl = "https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/login/";
-  
-      fetch(corsAnywhereUrl + targetUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-        },
-        body: JSON.stringify(loginData),
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.message === "Authentication successful") {
-          console.log("User logged in successfully", data);
-          window.location.href = '/html/user_page.html';
-        } else {
-          console.error("Login error", data);
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const loginData = {
+            username_or_email: formData.get("username_or_email").toLowerCase(),
+            password: formData.get("password"),
+        };
+
+        const targetUrl = "https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/login/";
+
+        fetch(targetUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                "X-Requested-With": "XMLHttpRequest",
+            },
+            body: JSON.stringify(loginData),
+            credentials: 'same-origin' // Include credentials in the request
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.message === "Authentication successful") {
+                console.log("User logged in successfully", data);
+                window.location.href = '/html/user_page.html';
+            } else {
+                console.error("Login error", data);
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
     });
-  });
-  
+});
+
--- js/user_page.js
+++ js/user_page.js
@@ -1,44 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Fetch user profile data
     fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile/', {
-        credentials: 'include'  // Include credentials in the request
+        credentials: 'same-origin' // Include credentials in the request
     })
-      .then(response => response.json())
-      .then(data => {
-        if (data.username) {
-          // Display username, email, and products
-          document.getElementById('username').textContent = data.username;
-          document.getElementById('email').textContent = data.email;
-          // Assuming you have a div for products
-          const productsDiv = document.getElementById('products');
-          data.products_for_sale.forEach(product => {
-            // Create and append elements for each product
-            // Modify this according to how you want to display products
-            const productElement = document.createElement('div');
-            productElement.textContent = product.name; // Adjust based on your product structure
-            productsDiv.appendChild(productElement);
-          });
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  });
-  
+        .then(response => response.json())
+        .then(data => {
+            if (data.username) {
+                // Display username, email, and products
+                document.getElementById('username').textContent = data.username;
+                document.getElementById('email').textContent = data.email;
+                // Assuming you have a div for products
+                const productsDiv = document.getElementById('products');
+                data.products_for_sale.forEach(product => {
+                    // Create and append elements for each product
+                    // Modify this according to how you want to display products
+                    const productElement = document.createElement('div');
+                    productElement.textContent = product.name; // Adjust based on your product structure
+                    productsDiv.appendChild(productElement);
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
 
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/check-session', {
-        credentials: 'include'
+        credentials: 'same-origin' // Include credentials in the request
     })
-      .then(response => response.json())
-      .then(data => {
-        if (data.status !== 'authenticated') {
-          window.location.href = '/html/login.html';
-        }
-        // User is authenticated, continue with page-specific logic
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  });
-  
+        .then(response => response.json())
+        .then(data => {
+            if (data.status !== 'authenticated') {
+                window.location.href = '/html/login.html';
+            }
+            // User is authenticated, continue with page-specific logic
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
