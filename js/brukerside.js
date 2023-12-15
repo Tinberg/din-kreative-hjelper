a4db0e6791eb33d3692bc815ba2e1118ff1e3a29
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-15 13:34:14 +0100

    form

--- js/brukerside.js
+++ js/brukerside.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", function () {
     const token = localStorage.getItem('jwt_token');
     if (!token) {
         redirectToLogin("Vennligst logg inn for å se din profil.");
+        return;
     }
 
     const headers = new Headers({
@@ -9,29 +10,29 @@ document.addEventListener("DOMContentLoaded", async function () {
         "Authorization": `Bearer ${token}`
     });
 
-    try {
-        const response = await fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile', { headers });
-
-        if (!response.ok) {
-            throw new Error('Autentisering feilet, vennligst logg inn på nytt.');
-        }
-
-        const data = await response.json();
-
-        if (data.username) {
-            document.getElementById('username').textContent = data.username;
-            document.getElementById('email').textContent = data.email;
-            const productsDiv = document.getElementById('products');
-            data.products_for_sale.forEach(product => {
-                const productElement = document.createElement('div');
-                productElement.textContent = product.name;
-                productsDiv.appendChild(productElement);
-            });
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        redirectToLogin(error.message);
-    }
+    fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile', { headers })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Autentisering feilet, vennligst logg inn på nytt.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.username) {
+                document.getElementById('username').textContent = data.username;
+                document.getElementById('email').textContent = data.email;
+                const productsDiv = document.getElementById('products');
+                data.products_for_sale.forEach(product => {
+                    const productElement = document.createElement('div');
+                    productElement.textContent = product.name;
+                    productsDiv.appendChild(productElement);
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            redirectToLogin(error.message);
+        });
 
     function redirectToLogin(message) {
         localStorage.setItem('redirectMessage', message);
--- js/logginn.js
+++ js/logginn.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".login-form");
     const errorMessageContainer = document.getElementById('errorMessage');
     const redirectMessage = localStorage.getItem('redirectMessage');
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         localStorage.removeItem('redirectMessage');
     }
 
-    form.addEventListener("submit", async function (event) {
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
         errorMessageContainer.textContent = '';
 
@@ -20,26 +20,27 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         const targetUrl = "https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/jwt-auth/v1/token";
 
-        try {
-            const response = await fetch(targetUrl, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-                body: JSON.stringify(loginData),
+        fetch(targetUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(loginData),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Feil brukernavn, e-post eller passord. Vennligst prøv igjen.');
+                }
+                return response.json();
+            })
+            .then(data => {
+                localStorage.setItem('jwt_token', data.token);
+                window.location.href = '/html/brukerside.html';
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                errorMessageContainer.textContent = error.message;
             });
-
-            if (!response.ok) {
-                throw new Error('Feil brukernavn, e-post eller passord. Vennligst prøv igjen.');
-            }
-
-            const data = await response.json();
-            localStorage.setItem('jwt_token', data.token);
-            window.location.href = '/html/brukerside.html';
-        } catch (error) {
-            console.error("Error:", error);
-            errorMessageContainer.textContent = error.message;
-        }
     });
 });
