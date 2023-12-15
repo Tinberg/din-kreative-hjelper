681b72d0e6d89bffb2755ae1ecc9bdfb7df88984
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-15 09:45:23 +0100

    d

--- js/new_user.js
+++ js/new_user.js
@@ -1,42 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const form = document.querySelector(".create-user-form form");
 
-//     form.addEventListener("submit", function (event) {
-//         event.preventDefault();
-
-//         const formData = new FormData(form);
-//         const userData = {
-//             username: formData.get("username"),
-//             email: formData.get("email"),
-//             password: formData.get("password"),
-//         };
-
-//         const targetUrl = "https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/register/";
-
-//         fetch(targetUrl, {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 "Accept": "application/json",
-//                 "X-Requested-With": "XMLHttpRequest"
-//             },
-//             body: JSON.stringify(userData),
-//         })
-//         .then((response) => response.json())
-//         .then((data) => {
-//             if (data.user_id) {
-//                 console.log("User created successfully", data);
-//                 // Handle success (e.g., redirect to login page or show success message)
-//             } else {
-//                 console.error("Error creating user", data);
-//                 // Handle errors (e.g., show error message to the user)
-//             }
-//         })
-//         .catch((error) => {
-//             console.error("Error:", error);
-//         });
-//     });
-// });
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".create-user-form form");
 
@@ -65,11 +27,22 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             if (data.user_id) {
                 console.log("User created successfully", data);
-                // Redirect to login page or show success message
+                // Display a success message and a button
+                displaySuccessMessageAndButton("User created successfully. Please log in.");
             } else {
                 console.error("Error creating user", data);
+                // Handle error
             }
         })
         .catch(error => console.error("Error:", error));
     });
+
+    function displaySuccessMessageAndButton(message) {
+        const container = document.getElementById('successMessageContainer');
+        container.innerHTML = `
+            <p>${message}</p>
+            <button onclick="location.href='/html/login.html'">Go to Login</button>
+        `;
+        // Optionally, add styles to your message and button
+    }
 });
