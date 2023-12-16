1c3f2720961d5658b19fc7a2dae4394f1f83cde0
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-16 20:02:09 +0100

    aa

--- js/brukerside.js
+++ js/brukerside.js
@@ -1,135 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Retrieve the JWT token from localStorage
-//     const token = localStorage.getItem('jwt_token');
-    
-//     // Check if the JWT token exists (user is authenticated)
-//     if (!token) {
-//         redirectToLogin("Vennligst logg inn for å se din profil.");
-//         return;
-//     }
-
-//     // Set headers for the fetch request, including the JWT token
-//     const headers = new Headers({
-//         "Content-Type": "application/json",
-//         "Authorization": `Bearer ${token}`
-//     });
-
-//     // Fetch the user profile data from the server
-//     fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile', { headers })
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Autentisering feilet, vennligst logg inn på nytt.');
-//             }
-//             return response.json();
-//         })
-//         .then(data => {
-//             // Display the user's username and email
-//             document.getElementById('username').textContent = data.username;
-//             document.getElementById('email').textContent = data.email;
-                
-//             // Display the user's location
-//             const locationElement = document.getElementById("userLocation");
-//             locationElement.textContent = data.location || "Din posisjon er ikke angitt";
-
-//             // Event listener for updating location
-//             const updateButton = document.getElementById('updateLocationButton');
-//             if (updateButton) {
-//                 updateButton.addEventListener('click', function () {
-//                     const newLocation = document.getElementById('newLocation').value;
-//                     updateLocation(newLocation);
-//                 });
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//             redirectToLogin(error.message);
-//         });
-
-//     // Function to update user location
-//     function updateLocation(newLocation) {
-//         fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/update-location', {
-//             method: 'POST',
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 "Authorization": `Bearer ${token}`
-//             },
-//             body: JSON.stringify({ location: newLocation })
-//         })
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Failed to update location.');
-//             }
-//             return response.json();
-//         })
-//         .then(data => {
-//             // Update the location on the page and clear the input field
-//             const locationElement = document.getElementById("userLocation");
-//             locationElement.textContent = newLocation;
-//             document.getElementById('newLocation').value = '';
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//         });
-//     }
-
-//     // Function to redirect to the login page with a message
-//     function redirectToLogin(message) {
-//         localStorage.setItem('redirectMessage', message);
-//         window.location.href = '/html/logginn.html';
-//     }
-// });
-
-// //Function for GoogleMaps
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Function to initialize the Google Map
-//     function initMap(latitude, longitude) {
-//         var userLocation = { lat: latitude, lng: longitude };
-//         var map = new google.maps.Map(document.getElementById('map'), {
-//             zoom: 12,
-//             center: userLocation
-//         });
-//         new google.maps.Marker({
-//             position: userLocation,
-//             map: map
-//         });
-//     }
-
-//     // Function to display the user's location on the map
-//     function displayUserLocation(userLocation) {
-//         if (userLocation) {
-//             const locationParts = userLocation.split(', ');
-//             const latitude = parseFloat(locationParts[0]);
-//             const longitude = parseFloat(locationParts[1]);
-//             initMap(latitude, longitude);
-//         }
-//     }
-
-//     // Initialize the Autocomplete feature for the address input
-//     function initAutocomplete() {
-//         const autocomplete = new google.maps.places.Autocomplete(
-//             document.getElementById('newLocation'), {types: ['geocode']});
-
-//         autocomplete.addListener('place_changed', function() {
-//             const place = autocomplete.getPlace();
-//             if (!place.geometry) {
-//                 console.log("No details available for input: '" + place.name + "'");
-//                 return;
-//             }
-//             // Update location in the input field
-//             document.getElementById('newLocation').value = 
-//                 place.geometry.location.lat() + ', ' + place.geometry.location.lng();
-//         });
-//     }
-
-//     // Assuming you have user's location in a format "latitude, longitude"
-//     // For example, you can fetch it from your server or local storage
-//     const userLocation = '40.7128, -74.0060'; // Replace with actual data
-//     displayUserLocation(userLocation);
-
-//     initAutocomplete(); // Initialize the autocomplete feature
-// });
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const token = localStorage.getItem('jwt_token');
 
@@ -154,21 +22,12 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById('username').textContent = data.username;
         document.getElementById('email').textContent = data.email;
 
-        // Check if user location is available in localStorage
-        const userLocation = localStorage.getItem("userLocation");
-        if (userLocation) {
-            const locationParts = userLocation.split(', ');
+        // Use the user's location from the response
+        if (data.location) {
+            const locationParts = data.location.split(', ');
             const latitude = parseFloat(locationParts[0]);
             const longitude = parseFloat(locationParts[1]);
-
-            // Call convertCoordsToAddress to get the address
-            convertCoordsToAddress(latitude, longitude, function(address) {
-                const locationElement = document.getElementById("userLocation");
-                locationElement.textContent = address;
-
-                // Initialize the map with the retrieved coordinates
-                initMap(latitude, longitude);
-            });
+            initMap(latitude, longitude);
         }
 
         const updateButton = document.getElementById('updateLocationButton');
@@ -206,7 +65,6 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             const locationElement = document.getElementById("userLocation");
             locationElement.textContent = formattedAddress;
-
             if (currentCoordinates) {
                 const locationParts = currentCoordinates.split(', ');
                 const latitude = parseFloat(locationParts[0]);
--- js/nybruker.js
+++ js/nybruker.js
@@ -63,28 +63,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function initAutocomplete() {
         const locationInput = document.getElementById('location');
-        const autocomplete = new google.maps.places.Autocomplete(locationInput, { types: ['geocode'] });
+        const autocomplete = new google.maps.places.Autocomplete(locationInput, {types: ['geocode']});
 
-        autocomplete.addListener('place_changed', function () {
+        autocomplete.addListener('place_changed', function() {
             const place = autocomplete.getPlace();
             if (!place.geometry) {
                 console.log("No details available for input: '" + place.name + "'");
                 return;
             }
 
-            // Get the coordinates and formatted address
-            const latitude = place.geometry.location.lat();
-            const longitude = place.geometry.location.lng();
-            const formattedAddress = place.formatted_address;
-
-            // Call convertCoordsToAddress to get the address and update the input field
-            convertCoordsToAddress(latitude, longitude, function (address) {
-                locationInput.value = address;
-
-                // Update the userLocation in localStorage
-                const coords = latitude + ', ' + longitude;
-                localStorage.setItem("userLocation", coords);
-            });
+            locationInput.value = place.formatted_address;
+            const coords = place.geometry.location.lat() + ', ' + place.geometry.location.lng();
+            localStorage.setItem("userLocation", coords);
         });
     }
 
@@ -112,46 +102,33 @@ document.addEventListener("DOMContentLoaded", function () {
             },
             body: JSON.stringify(userData),
         })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.user_id) {
-                    messageContainer.innerHTML = `<p class="success-message">Bruker er opprettet. Vennligst logg inn.</p>`;
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.user_id) {
+                messageContainer.innerHTML = `<p class="success-message">Bruker er opprettet. Vennligst logg inn.</p>`;
+                
+                // Create a new button element
+                const loginButton = document.createElement("button");
+                loginButton.textContent = "Til innlogging";
+                loginButton.addEventListener("click", function() {
+                    window.location.href = '/html/logginn.html';
+                });
 
-                    // Create a new button element
-                    const loginButton = document.createElement("button");
-                    loginButton.textContent = "Til innlogging";
-                    loginButton.addEventListener("click", function () {
-                        window.location.href = '/html/logginn.html';
-                    });
+                // Append the button to the message container
+                messageContainer.appendChild(loginButton);
 
-                    // Append the button to the message container
-                    messageContainer.appendChild(loginButton);
-
-                    form.reset();
-                } else {
-                    let errorMessage = "En feil oppstod. Vennligst prøv igjen eller kontakt support hvis problemet vedvarer.";
-                    if (data.code === "user_exists") {
-                        errorMessage = "Brukernavn eller e-post eksisterer allerede. Vennligst prøv en annen.";
-                    }
-                    messageContainer.innerHTML = `<p class="error-message">${errorMessage}</p>`;
-                }
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-                messageContainer.innerHTML = `<p class="error-message">Vi opplever tekniske problemer. Vennligst prøv igjen senere eller kontakt support.</p>`;
-            });
-    });
-
-    function convertCoordsToAddress(lat, lng, callback) {
-        var geocoder = new google.maps.Geocoder();
-        var latlng = new google.maps.LatLng(lat, lng);
-        geocoder.geocode({ 'location': latlng }, function (results, status) {
-            if (status === 'OK' && results[0]) {
-                callback(results[0].formatted_address);
+                form.reset();
             } else {
-                console.error('Geocoder failed due to: ' + status);
-                callback("Unknown Address");
+                let errorMessage = "En feil oppstod. Vennligst prøv igjen eller kontakt support hvis problemet vedvarer.";
+                if (data.code === "user_exists") {
+                    errorMessage = "Brukernavn eller e-post eksisterer allerede. Vennligst prøv en annen.";
+                }
+                messageContainer.innerHTML = `<p class="error-message">${errorMessage}</p>`;
             }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            messageContainer.innerHTML = `<p class="error-message">Vi opplever tekniske problemer. Vennligst prøv igjen senere eller kontakt support.</p>`;
         });
-    }
+    });
 });
