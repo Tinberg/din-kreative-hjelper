c97d303631fa5a30e7754187cbdaf0a1a6f07168
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-18 11:10:19 +0100

    Update brukerside.js

--- js/brukerside.js
+++ js/brukerside.js
@@ -173,10 +173,188 @@
 
 
 //Profil page for logged in user. 
+// document.addEventListener("DOMContentLoaded", function () {
+//     const token = localStorage.getItem('jwt_token');
+
+//     //If no Token, redirect to login page. 
+//     if (!token) {
+//         redirectToLogin("Vennligst logg inn for å se din profil.");
+//         return;
+//     }
+
+//     const headers = new Headers({
+//         "Content-Type": "application/json",
+//         "Authorization": `Bearer ${token}`
+//     });
+
+//     fetchUserProfile();
+//     initAutocomplete();
+
+//     let tempCoordinates;
+//     let tempFormattedAddress;
+//     let currentCoordinates;
+
+//     //Fetches the profile of the currently logged-in user from the server
+//     function fetchUserProfile() {
+//         fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile', { headers })
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Autentisering feilet, vennligst logg inn på nytt.');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             document.getElementById('username').textContent = data.username;
+//             document.getElementById('email').textContent = data.email;
+
+//             // Fetch and display stored location
+//             fetchStoredLocation();
+
+//             const updateButton = document.getElementById('updateLocationButton');
+//             if (updateButton) {
+//                 updateButton.addEventListener('click', function () {
+//                     if (tempCoordinates && tempFormattedAddress) {
+//                         updateLocation(tempFormattedAddress);
+//                     }
+//                 });
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//             redirectToLogin(error.message);
+//         });
+//     }
+
+//     //Retrieves the stored location from localStorage and if available,
+
+//     function fetchStoredLocation() {
+//         const savedLocation = localStorage.getItem("userLocation");
+//         if (savedLocation) {
+//             const coords = parseCoordinates(savedLocation);
+//             if (coords) {
+//                 reverseGeocode(coords.latitude, coords.longitude)
+//                     .then(address => {
+//                         displayLocation(address, savedLocation);
+//                     })
+//                     .catch(error => {
+//                         console.error('Error fetching address:', error);
+//                     });
+//             }
+//         }
+//     }
+
+//     //Updates the user's location on the server.
+
+//     function updateLocation(formattedAddress) {
+//         currentCoordinates = tempCoordinates; // Update with the selected location
+//         localStorage.setItem("userLocation", currentCoordinates);
+//         displayLocation(formattedAddress, currentCoordinates);
+//     }
+
+//     //Converts geographic coordinates into a human-readable address using Google Maps Geocoding API
+
+//     function reverseGeocode(lat, lng) {
+//         return new Promise((resolve, reject) => {
+//             const geocoder = new google.maps.Geocoder();
+//             geocoder.geocode({ 'location': { lat, lng } }, function (results, status) {
+//                 if (status === 'OK') {
+//                     if (results[0]) {
+//                         resolve(results[0].formatted_address);
+//                     } else {
+//                         reject('No results found');
+//                     }
+//                 } else {
+//                     reject('Geocoder failed due to: ' + status);
+//                 }
+//             });
+//         });
+//     }
+
+//     //Redirect message
+//     function redirectToLogin(message) {
+//         localStorage.setItem('redirectMessage', message);
+//         window.location.href = '/html/logginn.html';
+//     }
+
+//     //Displays the user's location on the page and initializes a map centered at the location.
+//     function displayLocation(address, coordinates) {
+//         document.getElementById("userLocation").textContent = address;
+//         const coords = parseCoordinates(coordinates);
+
+//         if (coords) {
+//             initMap(coords.latitude, coords.longitude);
+//         } else {
+//             console.error('Invalid location format');
+//         }
+//     }
+
+//     // Parses a string containing latitude and longitude into a coordinates object. Returns a coordinates object if the string format is valid.
+
+//     function parseCoordinates(coordString) {
+//         const parts = coordString.split(', ');
+//         if (parts.length === 2) {
+//             const latitude = parseFloat(parts[0]);
+//             const longitude = parseFloat(parts[1]);
+//             if (!isNaN(latitude) && !isNaN(longitude)) {
+//                 return { latitude, longitude };
+//             }
+//         }
+//         return null;
+//     }
+
+//     //Show map based on userLocation
+//     function initMap(latitude, longitude) {
+//         if (isNaN(latitude) || isNaN(longitude)) {
+//             console.error("Invalid coordinates for map initialization");
+//             return;
+//         }
+
+//         const userLocation = { lat: latitude, lng: longitude };
+//         const map = new google.maps.Map(document.getElementById('map'), {
+//             zoom: 12,
+//             center: userLocation
+//         });
+//         new google.maps.Marker({
+//             position: userLocation,
+//             map: map
+//         });
+//     }
+
+//     //Initializes Google Maps Places Autocomplete on an input field.
+
+//     function initAutocomplete() {
+//         const autocomplete = new google.maps.places.Autocomplete(
+//             document.getElementById('newLocation'), { types: ['geocode'] });
+
+//         autocomplete.addListener('place_changed', function() {
+//             const place = autocomplete.getPlace();
+//             if (!place.geometry) {
+//                 console.log("No details available for input: '" + place.name + "'");
+//                 return;
+//             }
+
+//             const lat = place.geometry.location.lat();
+//             const lng = place.geometry.location.lng();
+//             if (isNaN(lat) || isNaN(lng)) {
+//                 console.error("Invalid place geometry");
+//                 return;
+//             }
+
+//             // Temporarily store the coordinates and formatted address
+//             tempCoordinates = lat + ', ' + lng;
+//             tempFormattedAddress = place.formatted_address || place.name;
+//         });
+//     }
+// });
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const token = localStorage.getItem('jwt_token');
 
-    //If no Token, redirect to login page. 
+    // If no Token, redirect to login page.
     if (!token) {
         redirectToLogin("Vennligst logg inn for å se din profil.");
         return;
@@ -194,7 +372,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let tempFormattedAddress;
     let currentCoordinates;
 
-    //Fetches the profile of the currently logged-in user from the server
+    // Fetches the profile of the currently logged-in user from the server
     function fetchUserProfile() {
         fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile', { headers })
         .then(response => {
@@ -207,8 +385,14 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById('username').textContent = data.username;
             document.getElementById('email').textContent = data.email;
 
-            // Fetch and display stored location
-            fetchStoredLocation();
+            // If location data is present, display it
+            if (data.location) {
+                const coords = parseCoordinates(data.location);
+                if (coords) {
+                    displayLocation(data.location, data.location); // Display using the location string
+                    initMap(coords.latitude, coords.longitude); // Initialize map with these coordinates
+                }
+            }
 
             const updateButton = document.getElementById('updateLocationButton');
             if (updateButton) {
@@ -225,71 +409,29 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    //Retrieves the stored location from localStorage and if available,
-
-    function fetchStoredLocation() {
-        const savedLocation = localStorage.getItem("userLocation");
-        if (savedLocation) {
-            const coords = parseCoordinates(savedLocation);
-            if (coords) {
-                reverseGeocode(coords.latitude, coords.longitude)
-                    .then(address => {
-                        displayLocation(address, savedLocation);
-                    })
-                    .catch(error => {
-                        console.error('Error fetching address:', error);
-                    });
-            }
-        }
-    }
-
-    //Updates the user's location on the server.
-
+    // Updates the user's location on the server.
     function updateLocation(formattedAddress) {
         currentCoordinates = tempCoordinates; // Update with the selected location
+        // Update the server with the new location
+        // Add code to update the location on the server
+        // ...
+
         localStorage.setItem("userLocation", currentCoordinates);
         displayLocation(formattedAddress, currentCoordinates);
     }
 
-    //Converts geographic coordinates into a human-readable address using Google Maps Geocoding API
-
-    function reverseGeocode(lat, lng) {
-        return new Promise((resolve, reject) => {
-            const geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ 'location': { lat, lng } }, function (results, status) {
-                if (status === 'OK') {
-                    if (results[0]) {
-                        resolve(results[0].formatted_address);
-                    } else {
-                        reject('No results found');
-                    }
-                } else {
-                    reject('Geocoder failed due to: ' + status);
-                }
-            });
-        });
-    }
-
-    //Redirect message
+    // Redirect message
     function redirectToLogin(message) {
         localStorage.setItem('redirectMessage', message);
         window.location.href = '/html/logginn.html';
     }
 
-    //Displays the user's location on the page and initializes a map centered at the location.
+    // Displays the user's location on the page and initializes a map centered at the location.
     function displayLocation(address, coordinates) {
         document.getElementById("userLocation").textContent = address;
-        const coords = parseCoordinates(coordinates);
-
-        if (coords) {
-            initMap(coords.latitude, coords.longitude);
-        } else {
-            console.error('Invalid location format');
-        }
     }
 
-    // Parses a string containing latitude and longitude into a coordinates object. Returns a coordinates object if the string format is valid.
-
+    // Parses a string containing latitude and longitude into a coordinates object.
     function parseCoordinates(coordString) {
         const parts = coordString.split(', ');
         if (parts.length === 2) {
@@ -302,7 +444,7 @@ document.addEventListener("DOMContentLoaded", function () {
         return null;
     }
 
-    //Show map based on userLocation
+    // Show map based on userLocation
     function initMap(latitude, longitude) {
         if (isNaN(latitude) || isNaN(longitude)) {
             console.error("Invalid coordinates for map initialization");
@@ -320,8 +462,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    //Initializes Google Maps Places Autocomplete on an input field.
-
+    // Initializes Google Maps Places Autocomplete on an input field.
     function initAutocomplete() {
         const autocomplete = new google.maps.places.Autocomplete(
             document.getElementById('newLocation'), { types: ['geocode'] });
