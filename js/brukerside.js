0115b42840158a7564f95804c2663570e74e7dda
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-18 21:22:17 +0100

    Update brukerside.js

--- js/brukerside.js
+++ js/brukerside.js
@@ -274,11 +274,22 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    function reverseGeocodeAndDisplay(lat, lng, location) {
+    function reverseGeocodeAndDisplay(lat, lng) {
         reverseGeocode(lat, lng)
             .then(address => {
-                // Create the formatted address as "city, country"
-                const formattedAddress = `${location.city}, ${location.country}`;
+                let city, country;
+    
+                // Iterate through the address components and extract city and country
+                address.forEach(component => {
+                    if (component.types.includes('locality')) {
+                        city = component.long_name;
+                    } else if (component.types.includes('country')) {
+                        country = component.long_name;
+                    }
+                });
+    
+                // Create the formatted address based on the extracted city and country
+                const formattedAddress = [city, country].filter(Boolean).join(', ');
     
                 displayLocation(formattedAddress, { lat, lng });
             })
@@ -286,6 +297,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error('Error fetching address:', error);
             });
     }
+    
     
 
     function redirectToLogin(message) {
