791603bd58152f38a1fcf1ba4115bfca93fc0123
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-18 13:53:17 +0100

    Update brukerside.js

--- js/brukerside.js
+++ js/brukerside.js
@@ -1,3 +1,173 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const token = localStorage.getItem('jwt_token');
+
+//     if (!token) {
+//         redirectToLogin("Vennligst logg inn for å se din profil.");
+//         return;
+//     }
+
+//     const headers = new Headers({
+//         "Content-Type": "application/json",
+//         "Authorization": `Bearer ${token}`
+//     });
+
+//     let userSelectedAddress = localStorage.getItem('user_selected_address'); // Store user-selected address
+
+//     fetchUserProfile();
+//     initAutocomplete();
+
+//     let tempCoordinates;
+//     let tempFormattedAddress;
+
+//     function fetchUserProfile() {
+//         fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/user-profile', { headers })
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Autentisering feilet, vennligst logg inn på nytt.');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             document.getElementById('username').textContent = data.username;
+//             document.getElementById('email').textContent = data.email;
+
+//             if (data.location) {
+//                 const coords = parseCoordinates(data.location);
+//                 if (coords) {
+//                     if (userSelectedAddress) {
+//                         displayLocation(userSelectedAddress, data.location); // Display user-selected address if available
+//                     } else {
+//                         reverseGeocodeAndDisplay(coords.latitude, coords.longitude, data.location);
+//                     }
+//                 }
+//             }
+
+//             const updateButton = document.getElementById('updateLocationButton');
+//             if (updateButton) {
+//                 updateButton.addEventListener('click', function () {
+//                     if (tempCoordinates && tempFormattedAddress) {
+//                         updateLocation(tempFormattedAddress);
+//                     }
+//                 });
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//             redirectToLogin(error.message);
+//         });
+//     }
+
+//     function updateLocation(formattedAddress) {
+//         displayLocation(formattedAddress, tempCoordinates);
+//         localStorage.setItem('user_selected_address', formattedAddress); // Save the user-selected address
+
+//         // Update the server with the new location
+//         const newLocationData = {
+//             location: tempCoordinates
+//         };
+    
+//         fetch('https://din-kreative-hjelper.cmsbackendsolutions.com/wp-json/myapp/v1/update-location', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': `Bearer ${token}`
+//             },
+//             body: JSON.stringify(newLocationData)
+//         })
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Failed to update location on server');
+//             }
+//             // Location updated successfully on the server
+//         })
+//         .catch(error => {
+//             console.error('Error updating location on server:', error);
+//         });
+//     }
+
+//     function reverseGeocode(lat, lng) {
+//         return new Promise((resolve, reject) => {
+//             const geocoder = new google.maps.Geocoder();
+//             geocoder.geocode({ 'location': { lat, lng } }, function (results, status) {
+//                 if (status === 'OK') {
+//                     if (results[0]) {
+//                         resolve(results[0].formatted_address);
+//                     } else {
+//                         reject('No results found');
+//                     }
+//                 } else {
+//                     reject('Geocoder failed due to: ' + status);
+//                 }
+//             });
+//         });
+//     }
+
+//     function reverseGeocodeAndDisplay(lat, lng, location) {
+//         reverseGeocode(lat, lng)
+//             .then(address => {
+//                 displayLocation(address, location);
+//             })
+//             .catch(error => {
+//                 console.error('Error fetching address:', error);
+//             });
+//     }
+
+//     function redirectToLogin(message) {
+//         localStorage.setItem('redirectMessage', message);
+//         window.location.href = '/html/logginn.html';
+//     }
+
+//     function displayLocation(address, coordinates) {
+//         document.getElementById("userLocation").textContent = address;
+//         const coords = parseCoordinates(coordinates);
+//         if (coords) {
+//             initMap(coords.latitude, coords.longitude);
+//         }
+//     }
+
+//     function parseCoordinates(coordString) {
+//         const parts = coordString.split(', ');
+//         if (parts.length === 2) {
+//             const latitude = parseFloat(parts[0]);
+//             const longitude = parseFloat(parts[1]);
+//             return (!isNaN(latitude) && !isNaN(longitude)) ? { latitude, longitude } : null;
+//         }
+//         return null;
+//     }
+
+//     function initMap(latitude, longitude) {
+//         const userLocation = { lat: latitude, lng: longitude };
+//         const map = new google.maps.Map(document.getElementById('map'), {
+//             zoom: 12,
+//             center: userLocation
+//         });
+//         new google.maps.Marker({
+//             position: userLocation,
+//             map: map
+//         });
+//     }
+
+//     function initAutocomplete() {
+//         const autocomplete = new google.maps.places.Autocomplete(
+//             document.getElementById('newLocation'), { types: ['geocode'] });
+
+//         autocomplete.addListener('place_changed', function() {
+//             const place = autocomplete.getPlace();
+//             if (!place.geometry) {
+//                 console.log("No details available for input: '" + place.name + "'");
+//                 return;
+//             }
+
+//             const lat = place.geometry.location.lat();
+//             const lng = place.geometry.location.lng();
+
+//             tempCoordinates = lat + ', ' + lng;
+//             tempFormattedAddress = place.formatted_address || place.name;
+//         });
+//     }
+// });
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const token = localStorage.getItem('jwt_token');
 
@@ -11,7 +181,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "Authorization": `Bearer ${token}`
     });
 
-    let userSelectedAddress = localStorage.getItem('user_selected_address'); // Store user-selected address
+    let userSelectedAddress = localStorage.getItem('user_selected_address'); // Retrieve user-selected address from local storage
 
     fetchUserProfile();
     initAutocomplete();
@@ -35,7 +205,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const coords = parseCoordinates(data.location);
                 if (coords) {
                     if (userSelectedAddress) {
-                        displayLocation(userSelectedAddress, data.location); // Display user-selected address if available
+                        displayLocation(userSelectedAddress, data.location); // Use the stored address
                     } else {
                         reverseGeocodeAndDisplay(coords.latitude, coords.longitude, data.location);
                     }
@@ -59,7 +229,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function updateLocation(formattedAddress) {
         displayLocation(formattedAddress, tempCoordinates);
-        localStorage.setItem('user_selected_address', formattedAddress); // Save the user-selected address
+        localStorage.setItem('user_selected_address', formattedAddress);  // Save the user-selected address
 
         // Update the server with the new location
         const newLocationData = {
