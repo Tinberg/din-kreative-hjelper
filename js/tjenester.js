e260164bd0024aa56a84c0cf30b3f2c3d8c9b25b
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-20 21:22:33 +0100

    d

---
+++ js/tjenester.js
@@ -0,0 +1,30 @@
+//function to show all posts
+document.addEventListener("DOMContentLoaded", function () {
+    loadAndDisplayAllPosts();
+});
+
+function loadAndDisplayAllPosts() {
+    fetch('https://yourdomain.com/wp-json/myapp/v1/all-posts/', {
+        method: 'GET',
+        // No need for authorization if public
+    })
+    .then(response => response.json())
+    .then(posts => {
+        const postsContainer = document.getElementById('allPostsContainer');
+        postsContainer.innerHTML = '';
+
+        posts.forEach(post => {
+            const postElement = `
+                <div class="post">
+                    <h3>${post.title}</h3>
+                    <img src="${post.image_url}" alt="${post.title}">
+                    <p>${post.description}</p>
+                </div>
+            `;
+            postsContainer.innerHTML += postElement;
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
