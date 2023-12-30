5d7d58391f492e82144dc7beb6b66164434da68b
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-12-30 21:42:57 +0100

    d

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 // Copyright
 const currentYear = new Date().getFullYear();
 document.getElementById('copyrightYear').textContent = currentYear;
+
+//section2 categories submenu 
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.categories > li > div > button').forEach(function(button) {
+        button.addEventListener('click', function() {
+            var submenu = this.parentElement.nextElementSibling;
+            
+            if (submenu) { 
+                document.querySelectorAll('.submenu').forEach(function(sub) {
+                    if (sub !== submenu) {
+                        sub.classList.remove('reveal');
+                    }
+                });
+                submenu.classList.toggle('reveal');
+            } else {
+                console.error("Submenu not found!"); 
+            }
+        });
+    });
+});
